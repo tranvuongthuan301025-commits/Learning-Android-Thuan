3611cb95e1e199f56979edae96518662517a0b47
thuantran <1122thuantran@gmail.com>
2025-12-19 23:35:25 +0700

    bài tập trên lớp

---
+++ untitled/src/session_2_bt1.kt
@@ -0,0 +1,10 @@
+//Bài 1
+fun main(){
+    var age: Int = 12
+
+    if(age >= 18){
+        println("đã đủ tuổi")
+    }else{
+        println("chưa đủ tuổi")
+    }
+}
---
+++ untitled/src/session_2_bt2.kt
@@ -0,0 +1,6 @@
+//Bài 2
+fun main(){
+    var name: String? = "An"
+    var length = name?.length
+    println(length)
+}
